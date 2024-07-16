0f92686d39fdee07fd6afc7352a1c069892f3988
SmiletoSUN <tangxin.lv@outlook.com>
2024-07-16 15:38:01 +0800

    remove hardhat

--- hardhat/contracts/Payable.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract testPayable {
-    event Received(address, uint);
-
-    receive() external payable {
-        emit Received(msg.sender, msg.value);
-    }
-}
--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,27 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-toolbox");
+
+// The next line is part of the sample project, you don't need it in your
+// project. It imports a Hardhat task definition, that can be used for
+// testing the frontend.
+require("./tasks/faucet");
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
 
 
-const PRIVATE_KEY = "0xd157979dd88ccf836c593c85105e17b8406a48537f09e529fec4b719b4bf83a8";
-const words = 'member learn copper have vocal deal town square whisper click decade bargain';
-const address = '0x01eF9CCF31a205A8a055957103A779B52CaB6200';
-const Path = `m/44'/60'/0'/0/0`;
+const { API_URL, PRIVATE_KEY } = process.env;
 
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.17",
+  defaultNetwork: "localhost",
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/9a55fe1c381b48b38ab25684918f6e90",
+      url: API_URL,
       accounts: [PRIVATE_KEY],
       chainId: 11155111,
     },
-
     localhost: {
       url: 'http://127.0.0.1:8545',
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
       chainId: 31337,
-    },
+    }
   }
 };
